5f6f77cc69426bab74595bb2af1810ab4e00ac60
Philip S Tellis <philip@bluesmoon.info>
2008-11-29 12:03:43 +0530

    add skeletons for constructor, output, _process*, and implement clear_params

--- src/HTML/Template.js
+++ src/HTML/Template.js
@@ -1,26 +1,57 @@
 var HTML = HTML || {};
 
-var HTML.Template = function() {
-	function _create(tmpl_str) {
-        
+(function() {
+
+var _initialisers = {};
+
+var _params = {};
+
+var _process_var = function(attrs) {
+};
+
+var _process = function(m0, m1, m2) {
+	if(typeof m1 === "undefined") {
+		// TODO: Log error (throw error if strict)
+		return "";
+	} else if(m1.toUpperCase() === "VAR") {
+		// TODO: maybe parse m2 into an object
+		return _process_var(m2);
 	}
-    
-	function _param(obj) {
-		switch (typeof(obj)) {
-			case "object":
-				/* Set parameters */
-				break;
-			case "string":
-				/* Get parameter */
-				break;
-			case "undefined":
-				/* Get all parameters */
-				break;
+}
+
+HTML.Template = function(o) {
+	if(typeof o === "string") {
+		_initialisers.tmpl_string = o;
+	} else if(typeof o === "object") {
+		// TODO: check parameters inside o before assigning
+		_initialisers = o;
+	} else {
+		return undefined;
+	}
+
+	return this;
+};
+
+HTML.Template.prototype = {
+	param: function (o) {
+		if(typeof o === "object") {
+			/* Set parameters */
+		} else if(typeof o === "string") {
+			/* Get parameter */
+		} else if(typeof o === "undefined") {
+			/* Get all parameters */
+		} else {
+			// TODO: log an error
+			return "undefined";
 		}
+	},
+
+	output: function() {
+	},
+
+	clear_params() {
+		_params = {};
 	}
-    
-    return {
-        create: _create,
-        param: _param,        
-    };
-}();
+};
+
+})();
