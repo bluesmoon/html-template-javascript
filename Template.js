105c631330c2344fb9cadb52079e01819cf14e2e
Anant Narayanan <anant@kix.in>
2008-11-29 11:28:24 +0530

    Initial skeleton

---
+++ Template.js
@@ -0,0 +1,16 @@
+var HTML = HTML || {};
+
+var HTML.Template = function() {
+    function _create() {
+        
+    }
+    
+    function _param() {
+        
+    }
+    
+    return {
+      create: _create,
+      param: _param,        
+    };
+}();
